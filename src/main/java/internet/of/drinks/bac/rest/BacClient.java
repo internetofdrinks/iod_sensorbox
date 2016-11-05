86cad9f220dc9eb8e7269f99ad42a583c80dc9af
Martin Aulich <m.aulich@gmx.de>
2016-11-05 02:12:12 +0100

    Working version of BAC REST client

--- src/main/java/internet/of/drinks/bac/rest/BacClient.java
+++ src/main/java/internet/of/drinks/bac/rest/BacClient.java
@@ -1,19 +1,33 @@
 package internet.of.drinks.bac.rest;
 
 import feign.Feign;
+import feign.Logger;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+import feign.slf4j.Slf4jLogger;
 
 /**
  * Created by Martin on 05.11.2016.
  */
-public class BacClient {
+public class BacClient implements BacApi {
 
-    public static void main(String[] args) {
-        BacApi bacApi = Feign.builder()
+    private final BacApi bacApi;
+    public BacClient() {
+        bacApi = Feign.builder()
                 .decoder(new GsonDecoder())
                 .encoder(new GsonEncoder())
+                .logger(new Slf4jLogger())
+                .logLevel(Logger.Level.FULL)
                 .target(BacApi.class, "https://iodservice.herokuapp.com");
-        bacApi.post(new BacValue("4712", 0.5D));
+    }
+
+    @Override
+    public void post(BacValue value) {
+        bacApi.post(value);
+    }
+
+    public static void main(String[] args) {
+        BacClient bacClient = new BacClient();
+        bacClient.post(new BacValue("4712", 0.5D));
     }
 }
