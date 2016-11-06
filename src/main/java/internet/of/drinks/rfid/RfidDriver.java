a78b186ba416768eb09cfaa06664c267c4b25dbe
Martin Aulich <m.aulich@gmx.de>
2016-11-06 12:42:15 +0100

    Debouncing of RFID Reader

--- src/main/java/internet/of/drinks/rfid/RfidDriver.java
+++ src/main/java/internet/of/drinks/rfid/RfidDriver.java
@@ -7,72 +7,56 @@ import internet.of.drinks.IpConnection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Max Partenfelder on 05/11/2016.
  */
-public class RfidDriver implements BrickletNFCRFID.StateChangedListener, Runnable
-{
-    public static final long DEBOUNCE_TIME = 2000;
+public class RfidDriver implements BrickletNFCRFID.StateChangedListener, Runnable {
 
-    private String readerUID;
-
+    private final ScheduledExecutorService executor;
     private List<RfidListener> listeners = new ArrayList<>();
     private BrickletNFCRFID rfidBricklet;
 
     private String lastIdRead = "";
-    private long lastTimeRead = 0;
 
-    public RfidDriver(String readerUID)
-    {
-        this.readerUID = readerUID;
+    public RfidDriver(String readerUID) {
         rfidBricklet = new BrickletNFCRFID(readerUID, IpConnection.INSTANCE.getConnection());
         rfidBricklet.addStateChangedListener(this);
+        executor = Executors.newScheduledThreadPool(1);
         requestTag();
     }
 
-    public void addListener(RfidListener listener)
-    {
+    public void addListener(RfidListener listener) {
         this.listeners.add(listener);
     }
 
     @Override
-    public void stateChanged(short state, boolean idle)
-    {
+    public void stateChanged(short state, boolean idle) {
         String rfid = "";
-        if(idle)
-        {
-            requestTag();
+        if (idle) {
+            executor.schedule(this::requestTag, 100, TimeUnit.MILLISECONDS);
         }
 
-        if(state == BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY)
-        {
+        if (state == BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY) {
             BrickletNFCRFID.TagID tagID = null;
-            try
-            {
+            try {
                 tagID = rfidBricklet.getTagID();
-            }
-            catch (TimeoutException e)
-            {
-                e.printStackTrace();
-            }
-            catch (NotConnectedException e)
-            {
+            } catch (TimeoutException | NotConnectedException e) {
                 e.printStackTrace();
             }
 
-            for(int i = 0; i < tagID.tidLength; i++)
-            {
+            for (int i = 0; i < tagID.tidLength; i++) {
                 rfid += Integer.toHexString(tagID.tid[i]);
             }
 
-            if(!rfid.equals(this.lastIdRead) || this.lastTimeRead + DEBOUNCE_TIME < System.currentTimeMillis())
-            {
+            if (!rfid.equals(this.lastIdRead)) {
                 String s = "Found tag of type " + tagID.tagType +
                         " with ID [" + Integer.toHexString(tagID.tid[0]);
                 rfid = Integer.toHexString(tagID.tid[0]);
-                for(int i = 1; i < tagID.tidLength; i++)
-                {
+                for (int i = 1; i < tagID.tidLength; i++) {
                     s += " " + Integer.toHexString(tagID.tid[i]);
                     rfid += Integer.toHexString(tagID.tid[i]);
                 }
@@ -81,48 +65,29 @@ public class RfidDriver implements BrickletNFCRFID.StateChangedListener, Runnabl
                 System.out.println(s);
 
 
-                for(RfidListener listener : this.listeners)
-                {
+                for (RfidListener listener : this.listeners) {
                     listener.rfidEvent(rfid);
                 }
 
                 this.lastIdRead = rfid;
-                this.lastTimeRead = System.currentTimeMillis();
             }
         }
     }
 
-    public void requestTag()
-    {
-        try
-        {
+    public void requestTag() {
+        try {
             rfidBricklet.requestTagID(BrickletNFCRFID.TAG_TYPE_TYPE2);
-        }
-        catch (TimeoutException e)
-        {
-            e.printStackTrace();
-        }
-        catch (NotConnectedException e)
-        {
+        } catch (TimeoutException | NotConnectedException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void run()
-    {
-        while(true)
-        {
-            try
-            {
+    public void run() {
+        while (true) {
+            try {
                 rfidBricklet.requestTagID(BrickletNFCRFID.TAG_TYPE_TYPE2);
-            }
-            catch (TimeoutException e)
-            {
-                e.printStackTrace();
-            }
-            catch (NotConnectedException e)
-            {
+            } catch (TimeoutException | NotConnectedException e) {
                 e.printStackTrace();
             }
         }
